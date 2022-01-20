5126c77d7198727882847a7d6fc0c3b0b3baa732
Luuk Schukkink <luuk.schukkink@gmail.com>
2022-01-20 02:04:43 +0100

    error opgelost als je bericht verwijderde zonder attatchment

--- events/logs/messageDelete.js
+++ events/logs/messageDelete.js
@@ -1,12 +1,21 @@
-module.exports = (client,Discord,message) =>{
+module.exports = async (client,Discord,message) =>{
     const logchannel = client.channels.cache.get("933466078145822720")
     if(!message.author.bot){
-        const deleteMessageEmbed = new Discord.MessageEmbed()
+        if(message.attachments.toJSON()[0] != null){
+            const deleteMessageEmbed = new Discord.MessageEmbed()
                 .setColor("#FF0000")
                 .setTitle("chatlog")
-                .setDescription(`Message deleted in ${message.channel} of user ${message.author}\n\n "${message.content}" ${message.attachments.toJSON()[0]["url"]} `)
-                .setImage(message.attachments.toJSON()[0]["url"])
-        logchannel.send({embeds:[deleteMessageEmbed]})
+                .setDescription(`Message deleted in ${message.channel} of user ${message.author}\n\n "${message.content}" `)
+                .setImage(message.attachments.toJSON()[0]["url"]) 
+                logchannel.send({embeds:[deleteMessageEmbed]})
+        } else {
+            const deleteMessageEmbed = await new Discord.MessageEmbed()
+                .setColor("#FF0000")
+                .setTitle("chatlog")
+                .setDescription(`Message deleted in ${message.channel} of user ${message.author}\n\n "${message.content}" `) 
+                logchannel.send({embeds:[deleteMessageEmbed]})
+        }
+        
         if(message.attachments.toJSON()[1] != null){
             i = 0
             logchannel.send("there are multiple images attached")
