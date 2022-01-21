70e017aa1b6ef1d4975ff461941e44080fdfcaa3
Luuk Schukkink <luuk.schukkink@gmail.com>
2022-01-21 11:52:14 +0100

    logs voor veranderen van message toegevoegt

---
+++ events/logs/messageUpdate.js
@@ -0,0 +1,9 @@
+module.exports = async (client,Discord,message,newmessage) =>{
+    const logchannel = client.channels.cache.get("933466078145822720")
+    if(message.author.bot) return;
+    const changeMessageEmbed = new Discord.MessageEmbed()
+        .setColor("#FFF700")
+        .setTitle("chatlog")
+        .setDescription(`Message changed in ${message.channel} of user ${message.author}\n old message \n"${message.content}" \n\n new message \n "${newmessage.content}"`) 
+        logchannel.send({embeds:[changeMessageEmbed]})
+}
