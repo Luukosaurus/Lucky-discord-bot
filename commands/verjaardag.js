7309617ae3386b672d25810669767289e494185f
Luuk Schukkink <luuk.schukkink@gmail.com>
2022-01-22 00:23:57 +0100

    verjaardag save systeem toegevoegt

---
+++ commands/verjaardag.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+module.exports = {
+    name: 'verjaardag',
+    description: "voegt iemands vierjaardag toe aan het systeem",
+    execute(client, message, args , Discord){
+        if(args[0])
+        {
+            if(args[0] === "remove") {
+                console.log("remove brthay")
+            } else if (args[0] = "add" &&Number.isInteger(args[1]*args[1]) && Number.isInteger(args[2]*args[2]) ){
+                if(0<args[1] && args[1]<=31 && 0<args[2] && args[2]<=12){
+                    fs.readFile(`./verjaardagensaves/verjaardagen.json`, "utf8", (err, jsonString) => {
+                        if (err) {
+                            console.log("File read failed:", err);
+                            return;
+                        }
+                        const verjaardagenvars = JSON.parse(jsonString)
+                        verjaardagenvars[message.author.id] = {}
+                        verjaardagenvars[message.author.id].dag = args[1]
+                        verjaardagenvars[message.author.id].maand = args[2]
+                        const verjaardagenstring = JSON.stringify(verjaardagenvars,null,2);
+                            fs.writeFile(`./verjaardagensaves/verjaardagen.json`, verjaardagenstring,function (err) {
+                                if (err) {console.log(err)};
+                            });
+                    })
+                } else {
+                    console.log("foute datum")
+                }
+            } else {
+                console.log("fout commando")
+            }
+            
+        }
+    }
+}
