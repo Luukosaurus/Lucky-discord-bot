fc9c410caf13b9486016338a9337138de70c23cb
Luuk Schukkink <luuk.schukkink@gmail.com>
2022-01-24 13:42:22 +0100

    help command compleet gemaakt

--- commands/help.js
+++ commands/help.js
@@ -23,12 +23,31 @@ module.exports = {
                 )
                 message.channel.send({embeds:[helpModEmbed]});
             }
+            if (args[0] == "overig"){
+                const helpModEmbed = new Discord.MessageEmbed()
+                .setColor("#3042B1")
+                .setTitle("Lijst met overige commands")
+                .addFields(
+                    { name: 'Ping', value: '!ping', inline: true },
+                    { name: 'Ip', value: '!ip', inline: true },
+                    { name: '\u200B', value: '\u200B'},
+                    { name: 'Map', value: '!map', inline: true},
+                    { name: 'Suggestie', value: '!suggestie (tekst)',inline: true},
+                    { name: '\u200B', value: '\u200B'},
+                    { name: 'verjaardag toevoegen', value: '!verjaardag add (dag) (maand)', inline: true},
+                    { name: 'verjaardag verwijderen', value: '!verjaardag remove',inline: true},
+                    // { name: '\u200B', value: '\u200B'},
+                    // { name: "Herstart de bot", value: '!reloadbot',inline: true},
+                )
+                message.channel.send({embeds:[helpModEmbed]});
+            }
         } else {
             const helpEmbed = new Discord.MessageEmbed()
             .setColor("#3042B1")
             .setTitle("Lijst met commands")
             .addFields(
                 { name: 'Mod commands', value: '!help mod', inline: true },
+                { name: 'Overige command', value:"!help overig", inline: true}
             )
             message.channel.send({embeds:[helpEmbed]});
         }
