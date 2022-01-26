b1bb2acb249fa26ab32e160765a520fcba2ffad8
Luuk Schukkink <luuk.schukkink@gmail.com>
2022-01-26 14:45:04 +0100

    anti nitro scam links toegevoegt

--- events/guild/message.js
+++ events/guild/message.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 module.exports = (client, Discord , message) =>{
     const guildid = message.guild.id;
     const guildFile = guildid+".json"
+	const logchannel = client.channels.cache.get("933466078145822720")
     fs.readFile(`./server_data/${guildFile}`, "utf8", (err, jsonString) => {
         if (err) {
           console.log("File read failed:", err);
@@ -16,4 +17,15 @@ module.exports = (client, Discord , message) =>{
         const c = client.commands.get(command)
         if (c) c.execute(client,message, args , Discord , myGuildPrefixes);
     });
+    if (message.content.includes("https://") || message.content.includes("http://") || message.content.includes("www.")) {
+        if(message.content.includes("nitro")){
+			message.delete(1);
+			message.channel.send(`${message.author} Het is niet toegestaan om het woord "nitro" te gebruiken in combinatie met een link`)
+			const nitroMessageEmbed = new Discord.MessageEmbed()
+                .setColor("#FF0000")
+                .setTitle("linklog")
+                .setDescription(`Message deleted in ${message.channel} of user ${message.author} because chance of nitro \n\n "${message.content}" `) 
+                logchannel.send({embeds:[nitroMessageEmbed]})
+		}
+    }
 }
