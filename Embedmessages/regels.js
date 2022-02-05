47d8719b8ef455ae282e014430ffbdc6301ef5e6
Luuk Schukkink <luuk.schukkink@gmail.com>
2022-02-05 19:43:43 +0100

    spelling gefixed in regels

--- Embedmessages/regels.js
+++ Embedmessages/regels.js
@@ -17,30 +17,30 @@ module.exports = async (client,Discord) => {
                 -Niet adverteren (zoals self promotie of promotie van je server).
                 -Niet andermans informatie delen (Dit geldt voor persoons informatie zoals naam of IP en dergelijken).
                 -Discriminatie is niet toegestaan.
-                -Niet bedrijgen(zowel irl bedreigingen zoals DDOS als ingame).
+                -Niet bedreigen(zowel irl bedreigingen zoals DDOS als ingame).
                 -Niet scammen
                 -Niet bedelen bij spellers of staff.(Bijvoorbeeld vragen om keys van anderen spelers of rank van staff)
-                -Geen ongepaste gesprekken zoals role-play en dergelijken
+                -Geen ongepaste gesprekken zoals role-play en dergelijke
                 \u200B`},
                 {name:"Minecraft regels", value:`Dit zijn de regels die specifiek gelden voor de Minecraft server.
 
                 -Niet een hack client gebruiken. De uitzonderingen hiervan zijn: Badlion Client, Lunar client, Laby mod, Optifine, Schematics(geen auto build).
                 -Niet griefen of stelen.
                 -Niet bewust liegen tegen staff.
-                -Ongepaste bouweren zijn niet toegestaan(Dit wordt bepaald door staff dus hier is geen discussie over mogelijk)
+                -Ongepaste bouwwerken zijn niet toegestaan(Dit wordt bepaald door staff dus hier is geen discussie over mogelijk)
                 -Niet misbruik maken van bugs.
                 -Je bent niet welkom op de server met een ongepaste naam of skin.
                 -Niet de server proberen te laten crashen of laggen.
                 -Ingame spullen mogen niet door worden verkocht voor echt geld
-                -Het is niet toegestaan om alt accounts te gebruiken(Indien je met een alt inlogt wordt dit account verbannen en kan je er nog een straf op je hooft account aan over houden)
+                -Het is niet toegestaan om alt accounts te gebruiken(Indien je met een alt inlogt wordt dit account verbannen en kan je er nog een straf op je hoofd account aan over houden)
                 \u200B`},
                 {name:"Discord regels",value:`Dit zijn de regels die specifiek gelden voor de Discord server.
                 
                 -Niet onnodig staff taggen(tag alleen bij tijdsgebonden situaties).
-                -Geen overmaten gebruik van emoji's capslock of GIFs
+                -Geen overmatig gebruik van emoji's capslock of GIFs
                 -Geen discussies aan gaan met staff.(Maak hier een ticket voor aan)
                 \u200B`})
-                .setFooter("Overtreden van deze regels kan resulteren in permanente verbaning. De zwaarheid van de straf wordt door staff bepaald en kan flexibel zijn")
+                .setFooter("Overtreden van deze regels kan resulteren in permanente verbanning. De zwaarheid van de straf wordt door staff bepaald en kan flexibel zijn")
             ruleschannel.send({embeds:[discordRegelsEmbed]});
         }
     })
