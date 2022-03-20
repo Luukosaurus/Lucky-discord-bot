f93ea82b3bec71bb3de7584f8e539fe55b6336ec
Luuk Schukkink <luuk.schukkink@gmail.com>
2022-03-21 00:38:34 +0100

    ban kick en ping verwijdert omdat niet nuttig

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: "The bot will respond with pong",
-    execute(client, message, args , Discord){
-        message.channel.send(`pong!`);
-    }
-}
