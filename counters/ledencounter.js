8d5134767341b9b471f85d31cd20387dca331fc3
Luuk Schukkink <luuk.schukkink@gmail.com>
2022-04-17 19:21:07 +0200

    ticket systeem over gezet naar een actionbar in plaats van emoji's

--- counters/ledencounter.js
+++ counters/ledencounter.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 module.exports = async (client,Discord) => {
-    const datum = new Date()
     newString = "abc"
     const guild = client.guilds.cache.get("932313950828253244")
     const teltijd = 12
     teldone = false
     setInterval(() => {
+        const datum = new Date()
         if (teldone){
             teldone = false
         }else if( teltijd == datum.getHours()){
-            fs.readFile(`./data/ledencount.txt`, "utf8", async(err, String) => {
+            fs.readFile(`./data/ledencount.txt`, "utf8", (err, String) => {
                 if (err) {
                     console.log("File read failed:", err);
                     return;
